aeea51eee5e3ef2e884dc4bd3d8078e6b9a6e116
Abdulrahman Zurghani <abdoz@Abdulrahmans-MacBook-Pro.local>
2025-04-26 13:31:36 -0400

    added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import Loggedin from "./layout/Loggedin";
-import { Button, ConfigProvider } from "antd";
+import { useState } from "react";
+import Loggedin from "./layouts/Loggedin";
+import { Button, ConfigProvider, theme } from "antd";
+import "./App.scss";
 
 function App() {
+  const [isDarkmode, setIsDarkmode] = useState(false);
+  const darkModeToggle = () => {
+    setIsDarkmode(!isDarkmode);
+  };
   const componentA = <Button type="primary">Admin Component A</Button>;
   const componentB = <Button type="default">تجربة</Button>;
-  const componentC = <Button type="dashed">Student Component C</Button>;
+  const componentC = (
+    <Button type="dashed" onClick={darkModeToggle}>
+      Student Component C
+    </Button>
+  );
   return (
     <>
-      <Loggedin>
-        <div>{componentB}</div>
-      </Loggedin>
+      <ConfigProvider
+        theme={{
+          algorithm: isDarkmode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+          // token: { fontFamily: "Zain" },
+        }}
+      >
+        <div className={isDarkmode ? "App App--dark" : "App App--light"}>
+          <Loggedin>{componentC}</Loggedin>
+        </div>
+      </ConfigProvider>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { ConfigProvider, theme } from "antd";
 import "antd/dist/reset.css";
 import "./main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider
-      direction="rtl"
-      theme={{
-        algorithm: theme.darkAlgorithm,
-        // token: { fontFamily: "Zain" },
-      }}
-    >
-      <App />
-    </ConfigProvider>
+    <App />
   </StrictMode>
 );
