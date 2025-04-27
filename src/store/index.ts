fe64f379047578af80dad0f22ccea9442079db1e
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-04-26 22:45:08 -0400

    created user slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth.slice';
 import uiReducer from './ui.slice';
-
+import userReducer from './user.slice';
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         ui: uiReducer,
+        user: userReducer,
     },
 });
 
