60bf95007e6889aa7c3db7973778226f7cd76611
Scrolen <zachareakaawan2@gmail.com>
2025-05-20 18:12:55 -0400

    review changes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import DownloadModal from "../../components/ExportModal/DownloadModal";
-import AttendanceStatusTag from "../../components/StatusTag/AttendanceStatusTag";
 
 const Home: React.FC = () => {
   const { t } = useTranslation();
@@ -9,9 +7,6 @@ const Home: React.FC = () => {
     <div>
       <h1>{t("app.title")}</h1>
       <p>{t("app.description")}</p>
-      <AttendanceStatusTag status="present" />
-      <AttendanceStatusTag status="absent" />
-      <AttendanceStatusTag status="late" />
     </div>
   );
 };
