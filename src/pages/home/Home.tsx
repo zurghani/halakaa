40a9c7e73dc52b8e7c7b50336c6d79ceb398e9cd
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-05-19 23:17:03 -0400

    removed from home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import StudentInfo from "../../components/StudentInfo/StudentInfo";
 
 const Home: React.FC = () => {
   const { t } = useTranslation();
@@ -8,7 +7,6 @@ const Home: React.FC = () => {
     <div>
       <h1>{t("app.title")}</h1>
       <p>{t("app.description")}</p>
-      <StudentInfo></StudentInfo>
     </div>
   );
 };
