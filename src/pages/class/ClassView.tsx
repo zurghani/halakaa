f6e3c5d7bd153e521a7867940748ee2959ed6d9f
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-06-20 16:11:17 -0400

    code review

--- src/pages/class/ClassView.tsx
+++ src/pages/class/ClassView.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ClassDetailsCard from "../../components/ClassDetailsCard/ClassDetailsCard";
-import EnrolledStudents from "../../components/ClassDetailsCard/EnrolledStudents";
-
+import EnrolledStudents from "./EnrolledStudents/EnrolledStudents";
+import { useSetButtons } from "../../layouts/PageLayout/PageLayout";
+import { Button } from "antd";
 
 const ClassView: React.FC = () => {
-
+  const [count, setCount] = React.useState(0);
+  const { setButtons } = useSetButtons();
+  useEffect(() => {
+    setButtons([
+      <Button key="add" type="default" onClick={() => setCount(count + 1)}>
+        +
+      </Button>,
+      <Button key="search" type="default" onClick={() => setCount(count - 1)}>
+        -
+      </Button>,
+    ]);
+  }, [count]);
   return (
     <>
       <ClassDetailsCard />
