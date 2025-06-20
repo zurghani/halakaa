5e8867e70a36c8d4fe673c4d3973909586e6e053
Adnan-F-Hassan <adnan.f.Hassan@outlook.com>
2025-06-20 07:22:49 +0300

    init class overview page

---
+++ src/pages/class/ClassView.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ClassDetailsCard from "../../components/ClassDetailsCard/ClassDetailsCard";
+
+
+const ClassView: React.FC = () => {
+
+  return (
+    <>
+      <ClassDetailsCard />
+    </>
+  );
+};
+
+export default ClassView;
