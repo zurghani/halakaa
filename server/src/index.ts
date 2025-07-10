288c71584152a37d00f4131af31a1875d6467414
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-07-10 11:55:48 -0400

    get requesr

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,3 +13,18 @@ serve({
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
+
+app.get('/api/hello', (c) => {
+  return c.json({
+    ok: true,
+    message: 'Hello Hono!',
+  })
+})
+
+app.get('/posts/:id', (c) => {
+  const page = c.req.query('page')
+  const id = c.req.param('id')
+  c.header('X-Message', 'Hi!')
+  return c.text(`You want to see ${page} of ${id}`)
+})
+
