1f8e378ef5cc2cd47bfff86eabae9b5bdafdd7ff
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-07-10 23:44:57 -0400

    create student

--- server/src/modules/students/controller.ts
+++ server/src/modules/students/controller.ts
@@ -10,15 +10,8 @@ export const listStudents: Handler = async (c) => {
     const students = await studentsService.getAllStudents();
     return c.json(students);
 };
-
-// export const getBook = async (c) => {
-//     const id = c.req.param("id");
-//     const book = await bookService.getBookById(id);
-//     return c.json(book);
-// };
-
-// export const createBook = async (c) => {
-//     const body = await c.req.json();
-//     const result = await bookService.createBook(body);
-//     return c.json(result, 201);
-// };
+export const getStudent: Handler = async (c) => {
+    const id = c.req.param("id");
+    const student = await studentsService.getStudentById(parseInt(id));
+    return c.json(student);
+};
--- server/src/modules/students/routes.ts
+++ server/src/modules/students/routes.ts
@@ -3,9 +3,8 @@ import * as studentsController from "./controller";
 
 const students = new Hono();
 
-students.get("/", studentsController.listStudents); // GET /book
-students.post("/", studentsController.createStudent); // POST /book
-
-// book.get("/:id", studentsController.getStudent); // GET /book/:id
+students.get("/", studentsController.listStudents);
+students.get("/:id", studentsController.getStudent);
+students.post("/", studentsController.createStudent);
 
 export default students;
