94252e6423fcec9612cd982325d29e71cbe1609d
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-07-14 20:55:16 -0400

    added logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,17 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import routes from "./Routes";
-
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
-const db = drizzle(process.env.DATABASE_URL!);
 
 const app = new Hono();
+
+app.use("*", async (c, next) => {
+    console.log(`${c.req.method} ${c.req.url} [${new Date().toISOString()}]`);
+    console.log("Request Headers:", c.req.header);
+    console.log("Request Body:", await c.req.text());
+    console.log("\n****************************\n");
+    await next();
+});
 
 routes.forEach((route) => {
     app.route(route.route, route.handler);
