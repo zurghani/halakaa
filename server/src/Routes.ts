0acc891e2d60e404197fa74eb0acf53cc2937175
Scrolen <zachareakaawan2@gmail.com>
2025-07-22 01:51:52 -0400

    Roles api module

--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -2,12 +2,14 @@ import classes from "./modules/classes/routes";
 import enrollments from "./modules/enrollments/routes";
 import students from "./modules/students/routes";
 import attendances from "./modules/attendances/routes";
+import roles from "./modules/roles/routes"
 
 const routes = [
     { route: "/students", handler: students },
     { route: "/classes", handler: classes },
     { route: "/enrollments", handler: enrollments },
     { route: "/attendances", handler: attendances },
+    { route: "/roles", handler: roles },
 ];
 
 export default routes;
