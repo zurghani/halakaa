fc60f2e706525aefd0e3ca5bd9439dfe824b2034
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-08-02 21:18:27 -0400

    fixed icon direction in create user

--- client/src/pages/user/User.create.tsx
+++ client/src/pages/user/User.create.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Button, Form } from "antd";
-import { CaretLeftOutlined, CloseOutlined } from "@ant-design/icons";
-import { setCurrentPageTitle } from "../../store/ui.slice";
+import { CaretLeftOutlined, CaretRightOutlined, CloseOutlined } from "@ant-design/icons";
 import { Paths } from "../../Routes";
-import { useSetButtons } from "../../layouts/PageLayout/PageLayout";
+import { setCurrentPageTitle } from "../../store/ui.slice";
 import { UserForm } from "./components/UserForm";
+import { useSetButtons } from "../../layouts/PageLayout/PageLayout";
 
 const UserCreatePage: React.FC = () => {
     const navigate = useNavigate();
     const { setButtons } = useSetButtons();
     const dispatch = useDispatch();
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
     const [form] = Form.useForm();
 
     const handleSubmit = (values: any) => {
@@ -31,7 +31,9 @@ const UserCreatePage: React.FC = () => {
             <Button onClick={() => navigate(Paths.HOME.MAIN)} icon={<CloseOutlined />}>
                 {t("general.cancel")}
             </Button>,
-            <Button onClick={() => form.submit()} icon={<CaretLeftOutlined />}>
+            <Button
+                onClick={() => form.submit()}
+                icon={i18n.language == "en" ? <CaretRightOutlined /> : <CaretLeftOutlined />}>
                 {t("general.create")}
             </Button>,
         ]);
