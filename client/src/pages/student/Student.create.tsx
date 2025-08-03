e3f0fcd53a3c91575e3d3c5926073eda7feb1b8c
Adnan-F-Hassan <adnan.f.Hassan@outlook.com>
2025-08-03 19:30:03 +0300

    add create student modal

--- client/src/pages/student/Student.create.tsx
+++ client/src/pages/student/Student.create.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
-import { Button, Form } from "antd";
+import { Button, Form, Modal } from "antd";
 import { CaretLeftOutlined, CloseOutlined } from "@ant-design/icons";
 import { setCurrentPageTitle } from "../../store/ui.slice";
 import { Paths } from "../../Routes";
@@ -16,10 +16,19 @@ const StudentCreatePage: React.FC = () => {
     const { t } = useTranslation();
     const [form] = Form.useForm();
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const handleSubmit = (values: any) => {
         console.log("Submitted:", values);
         // actually create the student
+        setIsModalOpen(true);
     };
+
+    const handleDone = () => {
+        setIsModalOpen(false);
+        navigate(Paths.STUDENT.FIND); //redirects to students list page
+    };
+
     // Set Page Title
     useEffect(() => {
         dispatch(setCurrentPageTitle(t("titles.createUser")));
@@ -35,6 +44,26 @@ const StudentCreatePage: React.FC = () => {
             </Button>,
         ]);
     }, [t]);
-    return <StudentForm onSubmit={handleSubmit} form={form} />;
+    return (
+        <>
+        <StudentForm onSubmit={handleSubmit} form={form} />
+        <Modal
+            open={isModalOpen}
+            footer={[
+            <Button key="done" type="primary" onClick={handleDone}>
+            {t("modal.done")}
+            </Button>
+            ]}
+            onCancel={handleDone} // to close modal with Esc key
+            centered
+            closable={false}
+            maskClosable={false}
+            keyboard
+            title={t("modal.student.createSuccess")}
+          >
+            {t("modal.doneText")}
+          </Modal>
+        </>
+         );
 };
 export default StudentCreatePage;
