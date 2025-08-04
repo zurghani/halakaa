362ebacc6cb7f3377e3e0489ca9eff17e7672b61
Adnan-F-Hassan <adnan.f.Hassan@outlook.com>
2025-08-04 19:20:04 +0300

    add class modal

--- client/src/pages/class/Class.create.tsx
+++ client/src/pages/class/Class.create.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { Button, Form } from "antd";
+import { Button, Form, Modal } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
 import { Paths } from "../../Routes";
 import { setCurrentPageTitle } from "../../store/ui.slice";
@@ -16,10 +16,18 @@ const ClassCreatePage: React.FC = () => {
     const dispatch = useDispatch();
     const { t } = useTranslation();
     const [form] = Form.useForm();
+    
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleSubmit = (values: any) => {
         console.log("Submitted:", values);
         // Create class here
+        setIsModalOpen(true);
+    };
+
+    const handleDone = () => {
+        setIsModalOpen(false);
+        navigate(Paths.CLASS.FIND); //redirects to classes list page
     };
 
     // Set Page Title
@@ -35,6 +43,26 @@ const ClassCreatePage: React.FC = () => {
             <ActionButton onClick={() => form.submit()}>{t("general.create")}</ActionButton>,
         ]);
     }, [t]);
-    return <ClassForm form={form} onSubmit={handleSubmit} />;
+    return (
+        <>
+        <ClassForm form={form} onSubmit={handleSubmit} />
+        <Modal
+            open={isModalOpen}
+            footer={[
+            <Button key="done" type="primary" onClick={handleDone}>
+            {t("modal.done")}
+            </Button>
+            ]}
+            onCancel={handleDone} // to close modal with Esc key
+            centered
+            closable={false}
+            maskClosable={false}
+            keyboard
+            title={t("modal.class.createSuccess")}
+            >
+            {t("modal.doneText")}
+            </Modal>
+        </>
+    );
 };
 export default ClassCreatePage;
