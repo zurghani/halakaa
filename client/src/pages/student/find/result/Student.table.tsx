031a34e49c9de642c553340e431355517d4ca904
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-08-26 21:24:58 -0400

    random

--- client/src/pages/student/find/result/Student.table.tsx
+++ client/src/pages/student/find/result/Student.table.tsx
@@ -2,12 +2,9 @@ import { Table, TableProps } from "antd";
 import { Tag } from "antd";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { useTags } from "../../../../hooks/useTags";
 import { Student } from "../../../../types";
 
 const StudentTable = ({ students }: { students: Student[] }) => {
-
-    const { ageGroupTags } = useTags({});
     const { t } = useTranslation();
     const navigate = useNavigate();
 
@@ -20,21 +17,16 @@ const StudentTable = ({ students }: { students: Student[] }) => {
         {
             title: t("general.name"),
             dataIndex: "fullName",
-            key: "name",
+            key: "fullName",
             sorter: (a, b) => a.fullName.length - b.fullName.length,
         },
         {
             title: t("general.ageGroup"),
             dataIndex: "dateOfBirth",
-            key: "ageGroup",
-            render: (ageGroup: string) => ageGroupTags[ageGroup],
-            filters: [
-                { text: "5-10", value: "5-10" },
-                { text: "11-15", value: "11-15" },
-                { text: "16-20", value: "16-20" },
-            ],
-            onFilter: (value, record) =>
-                record.dateOfBirth ? record.dateOfBirth.includes(value as string) : false,
+            key: "dateOfBirth",
+            render: (record: string) => <Tag color="blue">{record}</Tag>,
+            sorter: (a, b) =>
+                a.dateOfBirth && b.dateOfBirth ? a.dateOfBirth.localeCompare(b.dateOfBirth) : 0,
         },
     ];
     return (
