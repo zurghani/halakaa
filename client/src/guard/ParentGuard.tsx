773ddff7f6edfcb5b4ae099823bd385840861d60
Abdulrahman Zurghani <abdozurghani@gmail.com>
2025-09-07 23:35:17 -0400

    Refactor ParentGuard and ViewStudent components to use authClient for user role management; remove unused user slice.

--- client/src/guard/ParentGuard.tsx
+++ client/src/guard/ParentGuard.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-// import { useSelector } from "react-redux";
-// import { AppStore } from "../store";
 import { Navigate, Outlet } from "react-router-dom";
 import { Paths } from "../Routes";
 import { UserRole } from "../store/types";
 import { authClient } from "../lib/auth-client";
 
 const ParentGuard: React.FC = () => {
-    //const userRole = useSelector((state: AppStore) => state.user.role);
+    const { data, isPending } = authClient.useSession();
 
-    const {data, isPending} = authClient.useSession()
-
-    if (isPending) return null
+    if (isPending) return null;
 
     if (data?.user.role !== UserRole.Parent) {
         return <Navigate to={Paths.HOME.ROOT} />;
